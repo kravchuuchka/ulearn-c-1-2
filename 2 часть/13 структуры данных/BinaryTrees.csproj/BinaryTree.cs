6adfe55855adc2216cb1d7174818a1dec2b2e533
kravchuchka <nastyakravchyuk@mail.ru>
2021-06-17 23:47:24 +0500

    second part

---
+++ 2 часть/13 структуры данных/BinaryTrees.csproj/BinaryTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        public T Value;
+        public BinaryTree<T> LeftTree;
+        public BinaryTree<T> RightTree;
+        public int Count;
+
+        public BinaryTree()
+        {
+
+        }
+
+        public BinaryTree(T element)
+        {
+            Value = element;
+        }
+
+        public void Add(T element)
+        {
+            if (Count == 0)
+            {
+                Value = element;
+                Count += 1;
+            }
+            else
+            {
+                var node = this;
+                var source = node;
+                Count += 1;
+                while (node != null)
+                {
+                    source = node;
+                    node = node.Value.CompareTo(element) < 0 ? node.RightTree : node.LeftTree;
+                    source.Count += 1;
+                }
+                if (source.Value.CompareTo(element) < 0)
+                {
+                    source.RightTree = new BinaryTree<T>(element);
+                    source.RightTree.Count += 1;
+                }
+                else
+                {
+                    source.LeftTree = new BinaryTree<T>(element);
+                    source.LeftTree.Count += 1;
+                }
+            }
+        }
+
+        public bool Contains(T element)
+        {
+            if (Count == 0) return false;
+            var node = this;
+            while (node != null)
+            {
+                if (node.Value.CompareTo(element) == 0) return true;
+                node = node.Value.CompareTo(element) < 0 ? node.RightTree : node.LeftTree;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 2 часть/13 структуры данных/DiskTree.csproj/DiskTreeTask.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskTree
+{
+    public class DiskTreeTask
+    {
+        private class Folder
+        {
+            private readonly string name;
+            public readonly List<Folder> InnerFolders;
+            public readonly string DepthInSpaces;
+
+
+            public Folder(string name)
+            {
+                this.name = name;
+                DepthInSpaces = "";
+                InnerFolders = new List<Folder>();
+            }
+
+            public Folder(string name, string parentDeepness)
+            {
+                this.name = name;
+                DepthInSpaces = parentDeepness + " ";
+                InnerFolders = new List<Folder>();
+            }
+
+            public Folder FindFolder(string folderName)
+            {
+                foreach (var e in InnerFolders)
+                {
+                    if (e.name == folderName) return e;
+                }
+                return default;
+            }
+
+            public List<string> ToList()
+            {
+                var result = new List<string>();
+                InnerFolders.Sort((folder1, folder2) =>
+                    string.Compare(folder1.name, folder2.name, StringComparison.Ordinal));
+
+                foreach (var folder in InnerFolders)
+                {
+                    result.Add($"{DepthInSpaces}{folder.name}");
+                    result.AddRange(folder.ToList());
+                }
+                return result;
+            }
+        }
+
+        public static List<string> Solve(List<string> input)
+        {
+            var root = new Folder("");
+            foreach (var folderPath in input)
+            {
+                var path = folderPath.Split('\\');
+                var node = root;
+                foreach (var name in path)
+                {
+                    var newFolder = node.FindFolder(name);
+                    if (newFolder != null)
+                    {
+                        node = newFolder;
+                        continue;
+                    }
+                    newFolder = new Folder(name, node.DepthInSpaces);
+                    node.InnerFolders.Add(newFolder);
+                    node = newFolder;
+                }
+            }
+            return root.ToList();
+        }
+    }
+}
